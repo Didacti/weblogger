c2d17ab7bf4cbb15e80086879fc8eab71253ca0c
Ivo Tzvetkov <ivotkv@gmail.com>
2018-01-11 11:59:17 -0500

    initial commit of weblogger

---
+++ build-venv.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -e
+set -x
+
+cd $(dirname "${BASH_SOURCE[0]}")
+
+PYTHON=${PYTHON:-"python"}
+VENV="venv"
+
+# build clean virtualenv
+rm -rf $VENV
+virtualenv --python=$PYTHON $VENV
+source $VENV/bin/activate
+
+# install standard packages
+pip install --upgrade pip
+pip install ipython
+pip install pytest
+
+# install dependencies
+pip install PyYAML 
+pip install argparse
+pip install tornado
+pip install httpagentparser
+
+# install drivers
+pip install udplogger
+pip install redmsg
